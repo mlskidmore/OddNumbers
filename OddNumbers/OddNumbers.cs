25fdf555cad392d08b7cd980cb9c451991a7f037
Mark Skidmore <mark.skidmore@hotmail.com>
2018-06-13 06:47:07 -0600

    Refactored code, put into a class

---
+++ OddNumbers/OddNumbers.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace OddNumbers
+{
+    class OddNumbers
+    {
+        public void getLowerBound(ref int lb)
+        {
+            Console.WriteLine("Enter an integer between 1 and 100,000:");
+            lb = Convert.ToInt32(Console.ReadLine());
+
+            while (lb < 1 || lb > 100000)
+            {
+                Console.WriteLine("You must enter an integer between 1 and 100,000:");
+                lb = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+        public void getUpperBound(ref int rb, int lb)
+        {
+            Console.WriteLine("Enter an integer between 1 and 100,000 and greater than the lower bound:");
+            rb = Convert.ToInt32(Console.ReadLine());
+
+            while (rb < lb || rb > 100000)
+            {
+                Console.WriteLine("You must enter an integer between 1 and 100,000 and greater than the lower bound:");
+                rb = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+
+        public void getOddNumbers(ref int[] oddNums, int lb, int rb)
+        {
+            int ii = 0;
+
+            for (int i = lb; i <= rb; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    oddNums[ii] = i;
+                    ii++;
+                }
+            }
+        }
+
+        public void printOddNumbers(int[] oddNums)
+        {
+            foreach (int i in oddNums)
+                Console.WriteLine(i);
+        }
+    }
+}
--- OddNumbers/Program.cs
+++ OddNumbers/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OddNumbers
 {
@@ -10,6 +6,31 @@ namespace OddNumbers
     {
         static void Main(string[] args)
         {
+            int lowerBound = 0, upperBound = 0;
+
+            OddNumbers on = new OddNumbers();
+
+            try
+            {
+                on.getLowerBound(ref lowerBound);
+                on.getUpperBound(ref upperBound, lowerBound);
+
+                Console.WriteLine("Lower bound: " + lowerBound);
+                Console.WriteLine("Upper bound: " + upperBound);
+
+                int[] oddNumbers = new int[(lowerBound + upperBound)/2];
+
+                on.getOddNumbers(ref oddNumbers, lowerBound, upperBound);
+
+                on.printOddNumbers(oddNumbers);
+
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("You must enter an integer between 1 and 100,000: " + e.Message);
+                Console.ReadLine();
+            }
         }
     }
 }
